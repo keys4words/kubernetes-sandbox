7a425182e646de9551692365fbde8daeae8cd639
keys4words <keys4words@gmail.com>
2022-12-13 20:47:56 +0300

    add stepik pods

--- stepik/app/app.go
+++ stepik/app/app.go
@@ -11,6 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	fmt.Println("Server is running...")
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
